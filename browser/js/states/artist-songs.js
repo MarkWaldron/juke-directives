c5e937403bf5b21e0b0122f5322dad39cae2b4a6
MarkWaldron <markewaldron@gmail.com>
2015-12-08 16:56:16 -0500

    Finished Dynamic Directives

--- browser/js/states/artist-songs.js
+++ browser/js/states/artist-songs.js
@@ -1,6 +1,6 @@
 app.config(function ($stateProvider) {
 	$stateProvider.state('artist.songs', {
 		url: '/songs',
-		templateUrl: '/templates/artist-songs.html'
+		template: '<song-list songs="artist.songs"></song-list>'
 	});
 });
